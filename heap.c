98e3a6c1ae38c3800dfa9aa09bf676999dfa967d
repl.it user <replituser@example.com>
2020-10-25 17:25:18 +0000

    new try

--- heap.c
+++ heap.c
@@ -18,8 +18,8 @@ typedef struct Heap{
 
 
 void* heap_top(Heap* arrai){
+    if (arrai==NULL || arrai->heapArray==NULL) return NULL;
     void * auxVar= arrai->heapArray[0].data;
-
     return auxVar;
 }
 
