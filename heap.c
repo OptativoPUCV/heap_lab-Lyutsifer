3ba873a89819f5511ca0abb9858da304d1c9051e
repl.it user <replituser@example.com>
2020-10-25 20:17:33 +0000

    new try

--- heap.c
+++ heap.c
@@ -27,13 +27,34 @@ void* heap_top(Heap* arrai){
 
 
 void heap_push(Heap* arrai, void* data, int priority){
+
   if (arrai== NULL || arrai->heapArray== NULL || arrai->capac== 0 || data== NULL) return;
   if (arrai->capac == arrai->size){
     arrai->heapArray= (heapElem*) realloc (arrai->heapArray , arrai->capac * 2 *sizeof(heapElem) );
   }
-  arrai->heapArray[arrai->size-1].priority= priority;
-  arrai->heapArray[arrai->size-1].data= data;
-  
+  int posi= arrai->size-1;
+  arrai->heapArray[posi].priority= priority;
+  arrai->heapArray[posi].data= data;
+  int auxPrio2= arrai->heapArray[(posi - 1)/2].priority;
+
+  while (priority > auxPrio2){
+    //Nodo que guarda data del padre (pos-1)/2
+    heapElem * auxNodo = (heapElem*) malloc(sizeof(heapElem));
+    auxNodo->data= arrai->heapArray[(posi -1)/2].data;
+    auxNodo->priority= arrai->heapArray[(posi -1)/2].priority;
+    //Se reemplaza el nodo nuevo en la posicion del padre
+    arrai->heapArray [(posi-1) / 2].data= data;
+    arrai->heapArray [(posi-1) / 2].priority= priority;
+    //El padre pasa a la posicion del nuevo nodo, i = size-1
+    arrai->heapArray[(posi)].priority = auxNodo->priority;
+    arrai->heapArray[(posi)].data = auxNodo->data;
+    //Nueva posicion del nodo
+    posi= (posi-1) / 2;
+    if (posi<0 || (posi-1 /2) < 0) break;
+    auxPrio2= arrai->heapArray[(posi - 1)/2].priority;
+     
+  }
+  return;
 }
 
 
