2005f2ca2ba73cbdcdf43f46bae396da009858bf
repl.it user <replituser@example.com>
2020-10-26 00:07:36 +0000

    new try

--- heap.c
+++ heap.c
@@ -27,7 +27,7 @@ void* heap_top(Heap* hip){
 
 
 void heap_push(Heap* hip, void* data, int priority){
-  
+
   if (hip== NULL || hip->heapArray== NULL || hip->capac== 0 || data== NULL) return;
   if (hip->capac == hip->size){
     hip->heapArray= (heapElem*) realloc (hip->heapArray , hip->capac * 2+1*sizeof(heapElem) );
@@ -69,26 +69,36 @@ void heap_pop(Heap* hip){
       hip->size--;
       return;
     }
-    switchNode(hip);
+    //switchNode(hip);
+    heapElem * n= (heapElem*) malloc (sizeof(heapElem));
+    n->data= hip->heapArray[0].data;
+    n->priority= hip->heapArray[0].priority;
+
+    hip->heapArray[0].priority= hip->heapArray[hip->size-1].priority;
+    hip->heapArray[0].data = hip->heapArray[hip->size-1].data;
+  
+    hip->heapArray[hip->size-1].priority= n->priority;
+    hip->heapArray[hip->size-1].data= n->data;
+
+    hip->size--;
     heapElem * nodeAux= (heapElem*) malloc (sizeof(heapElem));
 
     int posi= 0;
-    //FormulaHijos 2x+1 / 2x+2         StandBy (posi*2) +1 == (posi*2) +2
-    // (2*posi) + 1 < hip->size || (2*posi) + 2 < hip->size )
     while ( (2*posi) + 1 < hip->size || (2*posi) + 2 < hip->size ){ 
 
       nodeAux->priority= hip->heapArray[posi].priority;
       nodeAux->data= hip->heapArray[posi].data;
 
-      if ( nodeAux->priority < hip->heapArray[posi*2+1].priority ){
+      if (nodeAux->priority < hip->heapArray[posi*2+1].priority || nodeAux->priority < hip->heapArray[posi*2+2].priority){
+        if (hip->heapArray[posi*2+1].priority > hip->heapArray[posi*2+2].priority){
 
           hip->heapArray[posi].priority= hip->heapArray[posi*2 +1].priority;
           hip->heapArray[posi].data= hip->heapArray[posi*2 +1].data;
           hip->heapArray[posi*2 +1].priority= nodeAux->priority;
           hip->heapArray[posi*2 +1].data= nodeAux->data;
           posi= (posi*2)+1;
-      }
-      else if ( nodeAux->priority < hip->heapArray[posi*2+2].priority ){
+        } 
+        else if (hip->heapArray[posi*2+1].priority < hip->heapArray[posi*2+2].priority){
 
           hip->heapArray[posi].priority= hip->heapArray[posi*2 +2].priority;
           hip->heapArray[posi].data= hip->heapArray[posi*2 +2].data;
@@ -98,7 +108,9 @@ void heap_pop(Heap* hip){
       }
       else break;
     }
+
     return;
+  }
 }
 
 Heap* createHeap(){
@@ -109,7 +121,7 @@ Heap* createHeap(){
     return nHeap;
 }
 
-void switchNode(Heap * hip){
+/*void switchNode(Heap * hip){
   heapElem * nodeAux= (heapElem*) malloc (sizeof(heapElem));
   nodeAux->data= hip->heapArray[0].data;
   nodeAux->priority= hip->heapArray[0].priority;
@@ -122,4 +134,4 @@ void switchNode(Heap * hip){
 
   hip->size--;
   return;
-}
+}*/
